400cf18d5850bda721c212b41520a7300e4148cf
Foxy <foxy.github@gmail.com>
2025-04-02 22:46:42 +0200

    Added airline service

---
+++ src/views/AirlineView.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import type { AirlineModel } from '@/models/airline.model';
+import { AirlineService } from '@/services/airline.service';
+import { ref } from 'vue';
+
+const airlines = ref<AirlineModel[]>()
+AirlineService.getAirlines()
+    .then(rsp => airlines.value = rsp.data)
+</script>
+
+<template>
+{{  airlines }}
+</template>
